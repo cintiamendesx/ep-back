1e4ba6c61a2c914bcff2a1d58e0dd951a4a7c323
cintiamendesx <cintiamendesx@gmail.com>
2022-07-16 14:49:49 -0300

    nextconfig6

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     // Não esquecer de criar variável de ambiente com endereço do seu servidor Mongo local em desenvolvimento, e o endereço do cluster do Atlas em produção
-    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    const connection = await mongoose.connect("mongodb+srv://cintiamendesx:4514Emme@ironlibrary.42595.mongodb.net/test", {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
