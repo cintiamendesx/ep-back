64a84921b863024658a66fe539c5d31c3b4c3075
cintiamendesx <cintiamendesx@gmail.com>
2022-07-16 14:32:46 -0300

    nextconfig5

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
     app.use(cors());
     next();
 });
