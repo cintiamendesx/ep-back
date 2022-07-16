da1d45a1a7fbc738c01869d1c567d7efef210767
cintiamendesx <cintiamendesx@gmail.com>
2022-07-16 15:04:09 -0300

    nextconfig7

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ require("./config/db.config")();
 
 const app = express();
 
+app.use(cors())
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    app.use(cors());
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
 });
 
 
