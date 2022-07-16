8d71057d21d0c33ac3763ad39db5e0e6673b6fba
cintiamendesx <cintiamendesx@gmail.com>
2022-07-16 14:26:57 -0300

    nextconfig4

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ require("./config/db.config")();
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+
 app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 app.use(cors({ origin: "https://finalironlibrary.netlify.app" }));
